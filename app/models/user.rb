07c3add981b7afb10b7476f5d55134e61634eba9
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-23 08:58:18 -0600

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    # Validations
+
+    validates :username, presence: true, uniqueness:{ case_sensitive: false }, length: { minimum: 3, maximum: 30 }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    
+  end
+
+
+  # Associations
+
+ 
+  
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    username { "MyString" }
+    email { "MyString" }
+    password_digest { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+  end
+end
