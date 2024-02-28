5a0933b2e0dd1b7880fa08a007c1dacb0faeacbd
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-27 19:01:42 -0600

    added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,21 @@ class ApplicationController < ActionController::API
   
     private
   
+    # Authenticate incoming requests using JWT
     def authenticate_request
       header = request.headers['Authorization']
       header = header.split(' ').last if header
       begin
+        # Decode a token and set a current user.
         @decoded = jwt_decode(header)
         @current_user = User.find(@decoded[:user_id])
       rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+        # Handle unauthorized access
         render json: { errors: 'Unauthorized' }, status: :unauthorized
       end
     end
   
+    # Decode JWT token
     def jwt_decode(token)
       decoded = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
       HashWithIndifferentAccess.new decoded
