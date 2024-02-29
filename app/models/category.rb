eb688716331af1e56b8b301d26c388af03b00bb1
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-29 14:49:56 -0600

    added validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,11 @@
 class Category < ApplicationRecord
+
+  # Associations
     has_many :category_joins
     has_many :projects, through: :category_joins
+
+  # Validations
+    validates :category_name, presence: true
+    validates :description, presence: true
   end
   
