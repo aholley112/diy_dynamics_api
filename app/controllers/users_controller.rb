cd63a9772f6462f893a249d5ac41371fdd7a1bfb
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-29 11:17:31 -0600

    added comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,29 +2,37 @@ class UsersController < ApplicationController
     before_action :set_user, only: [:show, :update, :destroy]
     skip_before_action :authenticate_request, only: [:create]
   
+    # GET /users
     # List all users
+
     def index
-      users = User.all
-      render json: users, status: :ok
+      @users = User.all
+      render json: @users, status: :ok
     end
   
+    # GET /users/:id
     # Show a specific user
+
     def show
       render json: @user, status: :ok
     end
   
+    # POST /users
     # Create a new user
+
     def create
-      user = User.new(user_params)
+      @user = User.new(user_params)
   
-      if user.save
-        render json: user, status: :created
+      if @user.save
+        render json: @user, status: :created
       else
-        render json: user.errors, status: :unprocessable_entity
+        render json: @user.errors, status: :unprocessable_entity
       end
     end
   
+    # PUT /users/:id
     # Update a user
+
     def update
       if @user.update(user_params)
         render json: @user, status: :ok
@@ -33,6 +41,7 @@ class UsersController < ApplicationController
       end
     end
   
+    # DELETE /users/:id
     # Delete a user
     def destroy
       if @user.destroy
@@ -44,14 +53,16 @@ class UsersController < ApplicationController
   
     private
   
+    # Finds a user by id and sets it for the show, update and destroy actions
+
     def set_user
       @user = User.find(params[:id])
     end
   
+    # Defines parameters
+
     def user_params
       params.permit(:first_name, :last_name, :email, :username, :password)
     end
-    
-  
   end
   
