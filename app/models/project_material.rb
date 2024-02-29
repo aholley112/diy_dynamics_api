c47276ac49e30232356fbb2f94cb130ae204d84d
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-29 14:49:27 -0600

    added validations

--- app/models/project_material.rb
+++ app/models/project_material.rb
@@ -1,3 +1,15 @@
 class ProjectMaterial < ApplicationRecord
+
+  # Associations
+  
   belongs_to :project
+  belongs_to :material
+
+  # Validations
+
+  validates :quantity, presence: true
+  validates :price, presence: true
+  validates :project_id, presence: true
+  validates :material_id, presence: true
+
 end
