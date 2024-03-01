5d73c263b6a21cba95900eeb2323e2caba9e627e
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-29 20:43:06 -0600

    updated profiles tests

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe 'Profiles API', type: :request do
+RSpec.describe 'Profiles', type: :request do
   let!(:user) { create(:user) }
   let!(:profile) { create(:profile, user: user) }
   let(:user_id) { user.id }
   let(:token) { auth_token_for_user(user) }
   let(:headers) { { 'Authorization' => "Bearer #{token}" } }
 
+  # Test for GET. Show profile
   describe 'GET /users/:user_id/profile' do
     context 'when the profile does not exist' do
       before do
@@ -20,6 +21,7 @@ RSpec.describe 'Profiles API', type: :request do
     end
   end
 
+  # Test for POST. Create profile
   describe 'POST /users/:user_id/profile' do
     let(:valid_attributes) { { bio: 'New Bio' } }
 
@@ -41,6 +43,7 @@ RSpec.describe 'Profiles API', type: :request do
     end
   end
 
+  # Test for PUT. Update profile
   describe 'PUT /users/:user_id/profile' do
     let(:valid_attributes) { { bio: 'Updated Bio' } }
 
@@ -54,6 +57,7 @@ RSpec.describe 'Profiles API', type: :request do
     end
   end
 
+  # Test for DELETE. Delete profile
   describe 'DELETE /users/:user_id/profile' do
     before { delete "/users/#{user_id}/profile", headers: headers }
 
