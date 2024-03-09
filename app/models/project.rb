83c8738e47a6101692ee8257a930ad8d9c51710f
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-09 16:27:41 -0600

    corrected association names

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,10 +7,11 @@ class Project < ApplicationRecord
 has_and_belongs_to_many :categories, join_table: 'categories_projects'
 
   # Materials
-  has_and_belongs_to_many :tools, join_table: 'projects_tools'
+  has_and_belongs_to_many :materials, join_table: 'materials_projects'
 
   # Tools
-  has_and_belongs_to_many :tools, join_table: :projects_tools
+  has_and_belongs_to_many :tools, 
+  join_table: :projects_tools
 
   # Project Materials and Project Tools
   has_many :project_materials, dependent: :destroy
