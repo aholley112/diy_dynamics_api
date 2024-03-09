809e7223307644573f1eac8fb5663ee40103fc45
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-09 16:27:16 -0600

    added routes for categories and projects

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
       resource :profile, only: [:show, :create, :update, :destroy]
     end
 
-    # resources :projects do
-    #   member do
-    #     patch :toggle_favorite
-    #   end
-    # end
+  resources :projects 
   
     resources :project_materials
     resources :project_tools
 
     get 'categories/search', to: 'categories#search'
+
+    resources :categories do
+      get 'projects', to: 'projects#index_by_category'
+    end 
 
     resources :categories
     resources :materials
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
     post '/login', to: 'sessions#login'
     post '/signup', to: 'users#create'
     get '/profile', to: 'profiles#show_current_user'
+    get '/categories/:category_id/projects', to: 'projects#index_by_category'
   
   end
   
