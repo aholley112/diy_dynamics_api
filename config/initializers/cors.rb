539e41bbab35457e81d26898475a03aaf06afb52
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-13 19:22:25 -0500

    deployed project

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-    origins "*"
+    origins "https://diy-dynamics-client.vercel.app", "http://localhost:4200"
    
    resource "*",
     headers: :any,
