f99eac88ff21c33b73f70f9f97be8bc860eda38e
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-16 20:03:34 -0500

    adding profile picture

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,23 +1,22 @@
 class ProfilesController < ApplicationController
+  include Rails.application.routes.url_helpers  
   before_action :authenticate_request
   before_action :set_profile, only: [:update, :destroy]
 
-  # Shows the current user's profile or creates a default one if not present
   def show_current_user
     profile = @current_user.profile || create_default_profile_for(@current_user)
     render_profile_json(profile)
   end
 
-  # Updates the current user's profile
   def update
     if @profile.update(profile_params)
+      @profile.profile_picture.attach(profile_params[:profile_picture]) if profile_params[:profile_picture]
       render_profile_json(@profile)
     else
       render json: @profile.errors, status: :unprocessable_entity
     end
   end
 
-  # Creates a new profile for the user
   def create
     profile = @current_user.build_profile(profile_params)
     if profile.save
@@ -27,7 +26,6 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # Deletes the current user's profile
   def destroy
     @profile.destroy
     render json: { message: "Profile successfully deleted." }, status: :ok
@@ -41,7 +39,7 @@ class ProfilesController < ApplicationController
   end
 
   def profile_params
-    params.require(:profile).permit(:bio)
+    params.require(:profile).permit(:bio, :profile_picture)
   end
 
   def render_profile_json(profile)
@@ -49,10 +47,11 @@ class ProfilesController < ApplicationController
       id: profile.id,
       bio: profile.bio,
       user: {
-        firstName: @current_user.first_name, # Ensure these fields exist in your User model
+        firstName: @current_user.first_name,
         lastName: @current_user.last_name
       },
-      userId: @current_user.id
+      userId: @current_user.id,
+      profilePictureUrl: profile.profile_picture.attached? ? url_for(profile.profile_picture) : nil
     }
   end
 
