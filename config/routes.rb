188c901127ecb5018376b6f4264eb1699390bc6a
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-17 19:39:54 -0500

    added routes for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,39 @@
 Rails.application.routes.draw do
-  
-    resources :users do
-      resource :profile, only: [:show, :create, :update, :destroy]
-      post 'upload_image', on: :member
+  # User and Profile routes
+  resources :users do
+    resource :profile, only: [:show, :create, :update, :destroy]
+    member do
+      post 'upload_image'
     end
+  end
 
+  post '/login', to: 'sessions#login'
+  post '/signup', to: 'users#create'
+  get '/profile', to: 'profiles#show_current_user'
+
+
+  # Project routes
   resources :projects do
-  member do
-    post 'upload_image'
+    member do
+      post 'upload_image'
+      post 'add_to_favorites', to: 'favorites#create'
+      delete 'remove_from_favorites', to: 'favorites#destroy'
+    end
   end
+
+  get 'users/:user_id/favorites', to: 'favorites#index'
+  get 'favorites', to: 'favorites#index'
+
+
+  # Category routes
+  resources :categories do
+    get 'projects', to: 'projects#index_by_category', on: :member
+    get 'search', to: 'categories#search', on: :collection
+  end
+
+  # Other resources
+  resources :materials
+  resources :tools 
+  resources :project_materials
+  resources :project_tools
 end
-  
-    resources :project_materials
-    resources :project_tools
-
-    get '/categories/search', to: 'categories#search'
-
-    resources :categories do
-      get 'projects', to: 'projects#index_by_category'
-    end 
-
-    resources :categories
-    resources :materials
-    resources :tools 
-    
-    post '/login', to: 'sessions#login'
-    post '/signup', to: 'users#create'
-    get '/profile', to: 'profiles#show_current_user'
-    get '/categories/:category_id/projects', to: 'projects#index_by_category'
-  
-  end
-  
