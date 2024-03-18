293e27ce0c647b72c68982eca0e1eac390566c0c
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-17 19:39:32 -0500

    added favorites to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,8 +18,9 @@ has_and_belongs_to_many :categories, join_table: 'categories_projects'
   has_many :project_materials, dependent: :destroy
   has_many :project_tools, dependent: :destroy
   
+  has_many :favorites
+  has_many :favorited_by_users, through: :favorites, source: :user
 
-  
   # Validations
   validates :title, presence: true
   validates :description, presence: true
