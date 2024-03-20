e0cfa24578f199a09f82611f39f1527995ff40c6
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-20 09:20:55 -0500

    trying to fix deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-    origins "https://diy-dynamics-client-rmj8dp2i4-amandas-projects-ee19c902.vercel.app", "http://localhost:4200"
+        origins /.*\.vercel\.app$/, "http://localhost:4200", "http://localhost:4200"
    
    resource "*",
     headers: :any,
